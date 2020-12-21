a58a1ab48b6699ec5b17c3080f4efe0562619b9a
andregclvs <36642422+andregclvs@users.noreply.github.com>
2020-12-21 23:11:13 +0000

    statistics for location

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -1,5 +1,5 @@
 <script>
-  import { Bar } from 'vue-chartjs'
+  import { Bar, mixins  } from 'vue-chartjs'
 
   export default {
     /*
@@ -23,12 +23,19 @@
             3. provide / inject
     */
     extends: Bar,
+    mixins: [mixins.reactiveProp],
     /*
         Interface:
             1. inheritAttrs
             2. props
             3. emits
     */
+    props: {
+            options: {
+                type: Object,
+                default: null
+            }
+    },
     /*
         Composition API:
             1. setup
@@ -38,71 +45,6 @@
             1. data
             2. computed
     */
-    data() {
-      return {
-        chartData: {
-          labels: ["8h-9h", "9h-10h", "10h-11h", "11h-12h", "12h-13h", "13h-14h", "14h-15h", "15h-16h", "16h-17h",
-            "17h-18h", "18h-19h", "19h-20h"
-          ],
-          datasets: [{
-            label: 'Nº visitantes',
-            borderWidth: 1,
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)',
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-              'rgba(255,99,132,1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)',
-              'rgba(255,99,132,1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)'
-            ],
-            pointBorderColor: '#2554FF',
-            data: [12, 19, 3, 5, 2, 3, 20, 3, 5, 6, 2, 1]
-          }]
-        },
-        options: {
-          scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero: true
-              },
-              gridLines: {
-                display: true
-              }
-            }],
-            xAxes: [{
-              gridLines: {
-                display: false
-              }
-            }]
-          },
-          legend: {
-            display: true
-          },
-          responsive: true,
-          maintainAspectRatio: false
-        }
-      }
-    },
     /*
         Events:
             1. watch
@@ -126,11 +68,16 @@
     */
     mounted() {
       this.renderChart(this.chartData, this.options)
-    }
+    },
     /*
         Reactive Properties:
             1. methods
     */
+    methods: {
+            updateData() {
+                this.renderChart(this.chartData, this.options)
+            }
+    }
     /*
         Rendering:
             1. template / render
