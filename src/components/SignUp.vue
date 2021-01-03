66fcf3ff4ad2cd31a278069fb784943ce4b8d84a
André Gonçalves <36642422+andregclvs@users.noreply.github.com>
2021-01-03 11:56:00 +0000

    statistics for badge,collections and rewards (#98)

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login-box">
     <h2>{{language.pageTitle[selected_language]}}</h2>
-    <form @submit.prevent="submit">
+    <form @submit.prevent="submitForm">
       <div class="user-box">
         <input type="text" name="email" required="" v-model="form.email">
         <label>{{language.email[selected_language]}}</label>
@@ -10,6 +10,14 @@
         <input type="password" name="password" required="" v-model="form.password">
         <label>{{language.password[selected_language]}}</label>
       </div>
+      <div class="user-box">
+        <input type="radio" name="manager" value="manager" v-model="form.user_type" >
+        <label>Manager</label>
+      </div>
+       <div class="user-box">
+        <input type="radio" name="promoter" value="promoter" v-model="form.user_type" >
+        <label>Promoter</label>
+      </div>
       <div>
         <button
           class="submit_button"
@@ -17,12 +25,26 @@
           {{language.pageTitle[selected_language]}}
         </button>
       </div>
+      <div
+          class="alert failure"
+          v-if="error_banner"
+          v-on:click="error_banner=false;"
+      >
+          <strong>
+              {{language.failure_form.title[this.selected_language]}}
+          </strong>
+          {{language.failure_form.text[this.selected_language]}}
+          <br>
+          {{language.form_dismissal[this.selected_language]}}
+      </div>
     </form>
   </div>
 </template>
 
 <script>
 import firebase from "firebase";
+import store from '../store/index.js';
+import axios from 'axios';
 
 export default {
   /*
@@ -62,6 +84,30 @@ export default {
   data() {
     return {
       language: {
+        success_form: {
+            title: {
+                en: 'Success',
+                pt: 'Sucesso',
+            },
+            text: {
+                en: 'The information has been successfully submitted!',
+                pt: 'A informação foi submetida com sucesso!',
+            },
+        },
+        failure_form: {
+            title: {
+                en: 'Failure',
+                pt: 'Erro',
+            },
+            text: {
+                en: 'The information has not been successfully registed! Please try again!',
+                pt: 'A informação não foi registada com sucesso! Por favor tente de novo!',
+            },
+        },
+        form_dismissal: {
+            en: 'Click anywhere on the warning to dismiss it.',
+            pt: 'Clique no aviso para o remover.'
+        },
         pageTitle: {
           en: 'Sign Up',
           pt: 'Registo',
@@ -74,13 +120,20 @@ export default {
           en: 'Password',
           pt: 'Palavra-Passe',
         },
+        user_type: {
+          en: 'User type',
+          pt: 'Tipo de utilizador',
+        },
       },
       form: {
         email: "",
-        password: ""
+        password: "",
+        user_type:"",
       },
+      error_banner: false,
       error: null
     };
+    
   },
   computed: {
     selected_language() {
@@ -113,19 +166,62 @@ export default {
       1. methods
   */
   methods: {
-    submit() {
-      console.log("signupppp")
-      firebase
-        .auth()
-        .createUserWithEmailAndPassword(this.form.email, this.form.password)
-        .then(data => {
-          this.$router.replace({ name: "Home" });
-        })
-        .catch(err => {
-          this.error = err.message;
-        });
-    }
-  }
+    async submitForm(e) {
+                var data = this.form;
+                var postLink='';
+                console.log(data)
+
+                if (this.form.user_type=="manager") {
+                  postLink='http://localhost:8001/v0/users/managers';
+                }
+                if (this.form.user_type=="promoter") {
+                  postLink='http://localhost:8001/v0/users/promoters';
+                }
+
+                await axios.post(
+                    postLink, 
+                    data,
+                ).then(
+                    response => {
+                        console.log('response', response)
+                        if (response.status == 201) {
+                            this.success_banner = true;
+                            firebase
+                              .auth()
+                              .signInWithEmailAndPassword(this.form.email, this.form.password)
+                              .then(data => {
+                                console.log("Refresh Token", data.user.refreshToken);
+                                firebase
+                                .auth()
+                                .currentUser
+                                .getIdToken(true)
+                                .then(idToken => {
+                                  console.log("Id Token", idToken);
+                                  store.dispatch('setToken', idToken);
+                                  this.$router.replace({ name: "home" });
+                                })
+                                .catch(err1 => {
+                                  console.error(err1);
+                                  this.error_banner= true;
+                                });
+                              })
+                              .catch(err2 => {
+                                console.error(err2);
+                                this.error_banner= true;
+                              });
+                        }  
+                    }
+                ).catch(
+                    error => {
+                        console.log(error);
+                        this.error_banner= true;
+                        this.error= error;
+                    }
+                )
+
+                window.scrollTo(0,0);
+            }
+  },
   /*
       Rendering:
           1. template / render
@@ -190,7 +286,24 @@ html {
   font-size: 12px;
 }
 
+.alert {    
+    padding: 10px 5px;
+    margin: 0px auto;
+    border-radius: 8px;
+    font-size: 14px;
+    color: #333333;
+}
 
+
+.failure {
+    border: 1px solid #cb4444;
+    background-color: #d47575;
+}
+
+.failure:hover {
+    background-color: #b04c4c;
+    transition: 0.3s;
+}
 .submit_button {
     border: 1px solid #0a4870;
     border-radius: 5px;
